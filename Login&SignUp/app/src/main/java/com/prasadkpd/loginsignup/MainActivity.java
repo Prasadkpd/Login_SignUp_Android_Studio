08c9ae2fac8aa0cdcd4325012cd1f35a7e1ec6fd
Prasadkpd <2019cs078@stu.ucsc.lk>
2022-06-23 14:09:28 +0530

    login signup layout

---
+++ Login&SignUp/app/src/main/java/com/prasadkpd/loginsignup/MainActivity.java
@@ -0,0 +1,14 @@
+package com.prasadkpd.loginsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
